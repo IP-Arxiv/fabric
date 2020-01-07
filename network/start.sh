51e24b32923e29a8f43bd91ff7f8df28116a68f4
Dirk Hornung <dirkhornung91@gmail.com>
2020-01-07 10:13:42 +0100

    added ipa.journal affiliation on start

--- network/start.sh
+++ network/start.sh
@@ -16,4 +16,9 @@ docker exec -e "CORE_PEER_LOCALMSPID=IPAOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc
 # Join peer0.org1.example.com to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=IPAOrgMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org.ipa.com/msp" peer0.org.ipa.com peer channel join -b ipachannel.block
 
+# Enroll admin for fabric-ca-client
+docker exec ca.ipa.com fabric-ca-client enroll -u http://admin:adminpw@localhost:7054
 
+# Add fabric-ca org.ipa.journal affiliation
+docker exec ca.ipa.com fabric-ca-client affiliation add ipa
+docker exec ca.ipa.com fabric-ca-client affiliation add ipa.journal
